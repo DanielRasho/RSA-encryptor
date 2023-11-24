4a2d7dbc3aa233d78b037e77b94ab2f796e82fe5
Gerson Ramírez (Gerco) <93165594+Gxrco@users.noreply.github.com>
2023-11-23 20:20:26 -0600

    feat: Add GUI design and implementation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 # 37 characters alphabet
 from typing import Any, List
+from PyQt5 import QtWidgets, uic
+import sys
 
 
 ALPHABET = {"A": 0, "B": 1, "C": 2}
@@ -194,9 +196,111 @@ COMPLETE_ALPHABET = Alphabet(
     )
 )
 
-def main():
-    e = Encryptor()
+ALPHABET_CHOICES = [BASIC_ALPHABET, COMPLETE_ALPHABET]
 
+
+##########################
+## GUI IMPLEMENTATION
+##########################
+
+
+class RSASystem(QtWidgets.QMainWindow):
+    decryptor = Decryptor()
+    encryptor = Encryptor()
+
+    def __init__(self):
+        super(RSASystem, self).__init__()
+        uic.loadUi('UI.ui', self)
+        self.setUpEncrypted()
+        self.setUpComboBox()
+
+        self.btnOperate.clicked.connect(self.operate)
+        self.btnClean.clicked.connect(self.clean)
+        self.rbEncrypt.clicked.connect(self.setUpEncrypted)
+        self.rbDecrypt.clicked.connect(self.setUpDecrypted)
+        self.btnSTM.clicked.connect(self.sendText)
+
+    def operate(self):
+
+        if(self.rbEncrypt.isChecked()):
+            try:
+                msg = self.txtMessage.text().upper()
+                p = int(self.txtPrime1.text())
+                q = int(self.txtPrime2.text())
+                e = int(self.txtPrime3.text())
+                alphabet = ALPHABET_CHOICES[self.cmbAlphabet.currentIndex()]
+                result = self.encryptor.encrypt(msg, p, q, e, alphabet, 4)
+                self.txtResult.setText(result)
+            except ValueError:
+                self.showAlert("Invalid Input. Please enter valid numbers")
+            except:
+                self.showAlert("This action cannot be performed")
+
+        elif(self.rbDecrypt.isChecked()):
+            try:
+                msg = self.txtMessage.text().upper()
+                e = int(self.txtPrime1.text())
+                n = int(self.txtPrime2.text())
+                alphabet = ALPHABET_CHOICES[self.cmbAlphabet.currentIndex()]
+                result = self.decryptor.decrypt(msg, e, n, alphabet, 4)
+                self.txtResult.setText(result)
+            except ValueError:
+                self.showAlert("Invalid Input. Please enter valid numbers")
+            except:
+                self.showAlert("This action cannot be performed")
+        pass
+    
+    def sendText(self):
+        self.txtMessage.setText(self.txtResult.text())
+        self.txtResult.clear()
+    
+    def clean(self):
+        widgets_to_clear = [
+            self.txtPrime1, self.txtPrime2,
+            self.txtPrime3, self.txtResult, self.txtPrivKey
+        ]
+        for widget in widgets_to_clear:
+            widget.clear()
+
+    def setVisibility(self, label, widget, show):
+        label.setVisible(show)
+        widget.setVisible(show)
+
+    def setUpEncrypted(self):
+        self.clean()
+        self.lblResult.setText("Encrypted Message (C)")
+        self.setVisibility(self.lblPrivKey, self.txtPrivKey, False)
+        self.setVisibility(self.lblPrime3, self.txtPrime3, True)
+        self.lblPrime1.setText("Prime (p)")
+        self.lblPrime2.setText("Prime (q)")
+        self.lblPrime3.setText("Prime (e)")
+
+    def setUpDecrypted(self):
+        self.clean()
+        self.lblResult.setText("Decrypted Message (M)")
+        self.setVisibility(self.lblPrivKey, self.txtPrivKey, True)
+        self.setVisibility(self.lblPrime3, self.txtPrime3, False)
+        self.lblPrime1.setText("Prime (e)")
+        self.lblPrime2.setText("Prime (n)")
+
+    def setUpComboBox(self):
+        self.cmbAlphabet.addItems(["Basic","Complete"])
+        self.cmbAlphabet.setCurrentIndex(0)
+
+    def showAlert(self, description):
+        msg_box = QtWidgets.QMessageBox()
+        msg_box.setIcon(QtWidgets.QMessageBox.Critical)
+        msg_box.setWindowTitle("Error")
+        msg_box.setText(description)
+        msg_box.setStandardButtons(QtWidgets.QMessageBox.Ok)
+        msg_box.exec_()
+
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = RSASystem()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
